81b8946cd7051d4c3a34cfa84997fe84924e76dd
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-06-30 18:05:09 +0900

    add: database extension

---
+++ src/SimpleChat.Api/Extensions/DatabaseContextExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleChat.Api.Extensions
+{
+    using Microsoft.EntityFrameworkCore;
+    using SimpleChat.Data.Contexts;
+
+    public static class DatabaseContextExtension
+    {
+        private const string ConnectionStringName = "DefaultConnection";
+
+        public static void AddCustomSqliteContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration
+                .GetConnectionString(ConnectionStringName);
+
+            services.AddDbContext<ApplicationDbContext>(
+                o => o.UseSqlite(connectionString));
+
+            services.BuildServiceProvider()
+                .GetService<ApplicationDbContext>()
+                .Database
+                .Migrate(); // DB automigration on start enable
+        }
+    }
+}

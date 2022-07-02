4df814e2f992eee060b89c3446a0f5d4caed52d4
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-02 11:15:27 +0900

    change: minor changes

--- src/SimpleChat.Desktop/ViewModels/UserAuthorization/UserAuthorizationViewModel.cs
+++ src/SimpleChat.Desktop/ViewModels/UserAuthorization/UserAuthorizationViewModel.cs
@@ -2,19 +2,33 @@
 {
     using Prism.Mvvm;
     using Prism.Commands;
+    using Prism.Regions;
 
     public class UserAuthorizationViewModel : BindableBase
     {
+        private readonly IRegionManager _regionManager;
+
+        private string _title = null!;
+
+        public string Title
+        {
+            get => _title;
+
+            set => SetProperty(ref _title, value);
+        }
+
         public DelegateCommand ConnectCommand { get; set; } = null!;
 
-        public UserAuthorizationViewModel()
+        public UserAuthorizationViewModel(IRegionManager regionManager)
         {
+            _title = "User Authorization";
+            _regionManager = regionManager;
             InitializeCommand();
         }
 
         private void Connect()
         {
-
+            _regionManager.RequestNavigate("UserAuthorization", "ChatBase");
         }
 
         private void InitializeCommand()

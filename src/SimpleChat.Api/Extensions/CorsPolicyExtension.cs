0e3135e173b3b3ce54fb800b836dfa322267a09b
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-05 13:01:08 +0700

    add: cors policy extension

---
+++ src/SimpleChat.Api/Extensions/CorsPolicyExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleChat.Api.Extensions
+{
+    public static class CorsPolicyExtension
+    {
+        private const string SimpleChatCorsPolicy = "SimpleChatCorsPolicy";
+
+        private const string SectionName = "AllowedOrigins";
+
+        public static void AddCustomCorsPolicy(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddCors(o => o.AddPolicy(SimpleChatCorsPolicy, policy =>
+            {
+                string[] origins = configuration
+                    .GetSection(SectionName)
+                    .Get<List<string>>()
+                    .ToArray();
+
+                policy.WithOrigins(origins);
+                policy.AllowAnyHeader().AllowAnyMethod();
+            }));
+        }
+
+        public static void UseCustomCorsPolicy(this IApplicationBuilder app)
+        {
+            app.UseCors(SimpleChatCorsPolicy);
+        }
+    }
+}
